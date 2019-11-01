784c8967475ce0ec3b0c6bd8de28c510784137f9
kimhao <haolk@rikkeisoft.com>
2019-11-01 08:49:30 +0700

    first commit

---
+++ app/src/main/java/com/eric/vn/swipelayout/MainActivity.java
@@ -0,0 +1,24 @@
+package com.eric.vn.swipelayout;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.eric.vn.viewdraghelperdemo.R;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        (findViewById(R.id.delete_button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
